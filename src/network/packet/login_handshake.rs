381496d55f398d0b1f873cfbbc72531496c8ab05
Triston Plummer <me@triston.io>
2020-12-18 19:17:25 +0800

    Change from using C-represented structs, to a serializer trait.
    Signed-off-by: Triston Plummer <me@triston.io>

--- src/network/packet/login_handshake.rs
+++ src/network/packet/login_handshake.rs
@@ -1,4 +1,6 @@
 use rsa::{RSAPublicKey, PublicKeyParts};
+use crate::network::packet::SerializablePacket;
+use bytes::{BytesMut, BufMut};
 
 /// The opcode for a login handshake.
 pub const LOGIN_HANDSHAKE_OPCODE: u16 = 0xA101;
@@ -11,8 +13,7 @@ const MODULUS_LENGTH: u8 = 128;
 
 /// Represents a request from the login server to a client, to initiate a handshake and configure
 /// the AES encryption to use for further communication.
-#[repr(C, packed)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug)]
 pub struct LoginHandshakeRequest {
     opcode: u16,
     encrypted: bool,
@@ -29,7 +30,7 @@ impl LoginHandshakeRequest {
     pub fn new() -> LoginHandshakeRequest {
         LoginHandshakeRequest {
             opcode: LOGIN_HANDSHAKE_OPCODE,
-            encrypted: true,
+            encrypted: false,
             exponent_length: EXPONENT_LENGTH,
             modulus_length: MODULUS_LENGTH,
             exponent: [0; EXPONENT_LENGTH as usize],
@@ -52,6 +53,18 @@ impl LoginHandshakeRequest {
         request.exponent[..e.len()].clone_from_slice(e.as_slice());
         request.modulus[..n.len()].clone_from_slice(n.as_slice());
         return request;
+    }
+}
+
+/// Add support for serializing this packet over the network.
+impl SerializablePacket for LoginHandshakeRequest {
+    fn serialize(&self, writer: &mut BytesMut) {
+        writer.put_u16_le(self.opcode);
+        writer.put_u8(u8::from(self.encrypted));
+        writer.put_u8(self.exponent_length);
+        writer.put_u8(self.modulus_length);
+        writer.put_slice(&self.exponent);
+        writer.put_slice(&self.modulus);
     }
 }
 
--- src/network/packet/mod.rs
+++ src/network/packet/mod.rs
@@ -1,2 +1,10 @@
 mod login_handshake;
 pub use login_handshake::LoginHandshakeRequest;
+use bytes::BytesMut;
+
+/// A trait that a structure must implement if it wishes to be treated as a serializable packet.
+pub trait SerializablePacket {
+
+    /// Serializes the structure into a slice of bytes.
+    fn serialize(&self, writer: &mut BytesMut);
+}
